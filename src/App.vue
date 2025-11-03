ccea68d473b1a30256efbd682c94f9a7e18e76cc
MO7YW4NG <motywang@gmail.com>
2025-11-03 22:22:39 +0800

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <RouterView v-slot="{ Component }">
+    <KeepAlive include="HomeView">
+      <component :is="Component" />
+    </KeepAlive>
+  </RouterView>
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import SurroundingServiceView from '../views/SurroundingServiceView.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior() {
+    return { top: 0 };
+  },
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      meta: {
+        title: '申辦服務'
+      },
+      component: HomeView
+    },
+    {
+      path: '/surrounding-service',
+      name: 'surrounding-service',
+      meta: {
+        title: '周邊服務'
+      },
+      component: SurroundingServiceView
+    }
+  ]
+});
+
+// 導航守衛，用來動態修改標題
+router.beforeEach((to, from, next) => {
+  const defaultTitle = '城市通'; // 預設標題
+  document.title = (to.meta.title as string) || defaultTitle;
+  next();
+});
+
+export default router;
